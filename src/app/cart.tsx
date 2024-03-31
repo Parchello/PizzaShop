7c444642fe220067f0684c9900ee444c267ef33f
Parchello <parchellos@gmail.com>
2024-03-31 16:07:32 +0300

    total price added

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -2,16 +2,21 @@ import { View, Text, Platform, FlatList } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useCart } from "../provider/CartProvider";
 import CartListItem from "../components/CardListItem";
+import Button from "../components/Button";
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
   return (
-    <View>
+    <View style={{ padding: 10 }}>
       <FlatList
         data={items}
         renderItem={({ item }) => <CartListItem cartItem={item} />}
-        contentContainerStyle={{ padding: 10, gap: 10 }}
+        contentContainerStyle={{ gap: 10 }}
       />
+      <Text style={{ marginTop: 20, color: "white" }}>
+        Total price: ${total}
+      </Text>
+      <Button text="Checkout" />
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );

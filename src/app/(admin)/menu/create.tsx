47e714c6a01289fc3d9c028837a64f067f1f2904
Parchello <parchellos@gmail.com>
2024-04-01 14:38:29 +0300

    input and validation addded

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,21 +1,67 @@
 import Button from "@/src/components/Button";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { defaultImage } from "@/src/components/ProductListItem";
+import Colors from "@/src/constants/Colors";
+import { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Image } from "react-native";
 
 const CreateProductScreen = () => {
-  const onCreate = () => {
-    console.warn("Created product");
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const [errors, setErrors] = useState("");
+
+  const resetField = () => {
+    setName("");
+    setPrice("");
   };
+
+  const viladateInput = () => {
+    setErrors("");
+    if (!name) {
+      setErrors("Name is required");
+      return false;
+    }
+    if (!price) {
+      setErrors("Price is required");
+      return false;
+    }
+    if (isNaN(parseFloat(price))) {
+      setErrors("Price is required");
+      return false;
+    }
+    return true;
+  };
+
+  const onCreate = () => {
+    if (!viladateInput()) {
+      return;
+    }
+    console.warn("Created product", name, price);
+    //   тут буде збереження даних
+    resetField();
+  };
+
   return (
     <View style={styles.container}>
+      <Image source={{ uri: defaultImage }} style={styles.image} />
+      <Text style={styles.textButton}>Select Image</Text>
       <Text style={styles.label}>Pizza name</Text>
-      <TextInput placeholder="Pizza name" style={styles.input} />
+      <TextInput
+        placeholder="Pizza name"
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+      />
 
       <Text style={styles.label}>Price ($)</Text>
       <TextInput
         placeholder="9.99"
         style={styles.input}
         keyboardType="numeric"
+        value={price}
+        onChangeText={setPrice}
       />
+      <Text style={styles.error}>{errors}</Text>
       <Button text="Create product" onPress={onCreate} />
     </View>
   );
@@ -37,6 +83,21 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginTop: 5,
     marginBottom: 20,
+  },
+  error: {
+    color: "red",
+    fontSize: 20,
+  },
+  image: {
+    width: "50%",
+    aspectRatio: 1,
+    alignSelf: "center",
+  },
+  textButton: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: Colors.light.tint,
+    marginVertical: 10,
   },
 });
 

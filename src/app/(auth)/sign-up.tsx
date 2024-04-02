21bac4907214dcda5114c57ac8a6104097db3889
Parchello <parchellos@gmail.com>
2024-04-02 11:58:06 +0300

    auth compon add

---
+++ src/app/(auth)/sign-up.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Registration = () => {
+  return (
+    <View>
+      <Text>Registration</Text>
+    </View>
+  );
+};
+
+export default Registration;
